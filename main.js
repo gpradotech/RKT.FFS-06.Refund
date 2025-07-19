cc900bd5a393314beef11166e0be4688d3601357
contato.gprado@gmail.com <contato.gprado@gmail.com>
2025-07-18 22:01:17 -0300

    refatora estrutura de arquivos: substitui scripts.js por main.js e elements.js, ajustando importações

---
+++ main.js
@@ -0,0 +1,3 @@
+import { handleSubmit } from './scripts/form__Submit.js';
+import { formatAmount } from './scripts/form__FormatAmount.js';
+import { createListItem } from './scripts/list__CreateItem.js';
---
+++ scripts/elements.js
@@ -0,0 +1,6 @@
+export const form = document.querySelector('form');
+export const expense = document.getElementById('expense');
+export const category = document.getElementById('category');
+export const amount = document.getElementById('amount');
+
+export const list = document.querySelector('ul');
